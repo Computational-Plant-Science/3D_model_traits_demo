4c3ad17c54ab9a83b05eebeee51eb4cdfbb64f49
lsx1980 <suxingliu@gmail.com>
2021-11-04 19:35:06 -0400

    replace segmentation background as black

--- model_alignment.py
+++ model_alignment.py
@@ -150,7 +150,7 @@ def format_converter(current_path, model_name):
     hull, _ = pcd_r.compute_convex_hull()
     hull_ls = o3d.geometry.LineSet.create_from_triangle_mesh(hull)
     hull_ls.paint_uniform_color((1, 0, 0))
-    #o3d.visualization.draw_geometries([pcd_r, hull_ls, aabb, obb, o3d.geometry.TriangleMesh.create_coordinate_frame()])
+    o3d.visualization.draw_geometries([pcd_r, hull_ls, aabb, obb, o3d.geometry.TriangleMesh.create_coordinate_frame()])
     
     #print(hull.get_volume())
     
@@ -336,6 +336,11 @@ def format_converter(current_path, model_name):
     #downpcd = pcd_r.voxel_down_sample(voxel_size=0.5)
     #o3d.visualization.draw_geometries([downpcd])
     '''
+    
+    #print("Statistical oulier removal\n")
+    cl, ind = pcd_r.remove_statistical_outlier(nb_neighbors = 40, std_ratio = 0.00001)
+    display_inlier_outlier(pcd_r, ind)
+   
     
     ####################################################################
     
