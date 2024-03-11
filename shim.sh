2892bcfaecdc4ad2246d4ef28ade7d45a2c1296a
lsx1980 <suxingliu@gmail.com>
2024-03-11 13:22:21 -0400

    debug output folder path for plantit

--- shim.sh
+++ shim.sh
@@ -5,10 +5,5 @@ python3 /opt/code/pipeline.py -p $INPUT -o $OUTPUT
 find . -type f -name "*.xyz" -exec cp {} $WORKDIR \;
 find . -type f -name "*.ply" -exec cp {} $WORKDIR \;
 find . -type f -name "*.stl" -exec cp {} $WORKDIR \;
-<<<<<<< HEAD
 find . -type f -name "*.obj" -exec cp {} $WORKDIR \;
-=======
-find . -type f -name "*.xyz" -exec cp {} $WORKDIR \;
-find . -type f -name "*.ply" -exec cp {} $WORKDIR \;
->>>>>>> ea3ba77405bf516f9f3c54c27fcbc0099d420693
 find . -type f -name "*.xlsx" -exec cp {} $WORKDIR \;
